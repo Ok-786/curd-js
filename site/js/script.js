df5757a7c441fa19b921cb15e2180065951618ff
Ok-786 <osamakiyani6123@gmail.com>
2021-08-17 10:28:06 +0500

    asdsad

--- site/js/script.js
+++ site/js/script.js
@@ -1,3 +1,4 @@
+const getForm = document.getElementById("reg");
 const getFname = document.getElementById("fname");
 const getLname = document.getElementById("lname");
 const getEmail = document.getElementById("email");
@@ -8,36 +9,87 @@ var getLocalStorage = null;
 var getPendingTasks = document.querySelector(".pendingTasks");
 const clearallBtn = document.querySelector(".footer button");
 var selectGender = document.getElementById('gender').value;
+var getSubmitButton = document.querySelector(".btn");
+var getMessage = document.querySelector(".message");
+var passwordMatch = true;
+var tempIndex = null;
 arrEntry =[];
 
 displayUsers();
 
+function onKeyUp() {
+    
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=repassword]');
+    console.log(confirm.value);
+    console.log(password.value);
+    if (confirm.value === password.value) {
+        passwordMatch = true;
+        getMessage.classList.remove("unmatch");
+        getMessage.classList.add("match");
+        getMessage.innerHTML = "password match";
+        
+    } else {
+        passwordMatch = false;
+        getMessage.classList.remove("match");
+        getMessage.classList.add("unmatch")
+        getMessage.innerHTML = "password doesnt match"
+        
+    }
+    
+}
+
 onSubmit = (event) => {
+    
     event.preventDefault();
-    selectGender = document.getElementById('gender').value;
-    const dataEntry = {
-        firstName: getFname.value,
-        lastName: getLname.value,
-        email: getEmail.value,
-        password: getPassword.value,
-        rePassword: getRepassword.value,
-        gender: selectGender, 
+    if(passwordMatch == true) {
+        selectGender = document.getElementById('gender').value;
+        const dataEntry = {
+            firstName: getFname.value,
+            lastName: getLname.value,
+            email: getEmail.value,
+            password: getPassword.value,
+            rePassword: getRepassword.value,
+            gender: selectGender, 
+        }
+        if(getSubmitButton.value == "Submit"){
+            getLocalStorage = localStorage.getItem("Data Entry");
+
+            getLocalStorage==null? arrEntry=[] : arrEntry = JSON.parse(getLocalStorage)
+            arrEntry.push(dataEntry);
+
+            localStorage.setItem("Data Entry", JSON.stringify(arrEntry));
+
+            displayUsers();
+
+            getFname.value="";
+            getLname.value="";
+            getEmail.value="";
+            getPassword.value="";
+            getRepassword.value="";
+        }
+        else if(getSubmitButton.value == "Edit") {
+
+            getLocalStorage = localStorage.getItem("Data Entry");
+
+            getLocalStorage==null? arrEntry=[] : arrEntry = JSON.parse(getLocalStorage)
+            arrEntry.splice(tempIndex,1);
+            arrEntry.splice(tempIndex,0,dataEntry);
+
+            localStorage.setItem("Data Entry", JSON.stringify(arrEntry));
+
+            displayUsers();
+
+            getFname.value="";
+            getLname.value="";
+            getEmail.value="";
+            getPassword.value="";
+            getRepassword.value="";
+        }
+    } else {
+        // getRepassword.setCustomValidity("Passwords Don't Match");
     }
 
-    getLocalStorage = localStorage.getItem("Data Entry");
-
-    getLocalStorage==null? arrEntry=[] : arrEntry = JSON.parse(getLocalStorage)
-    arrEntry.push(dataEntry);
-
-    localStorage.setItem("Data Entry", JSON.stringify(arrEntry));
-
-    displayUsers();
-
-    getFname.value="";
-    getLname.value="";
-    getEmail.value="";
-    getPassword.value="";
-    getRepassword.value="";
 }
 
 function displayUsers()
@@ -48,13 +100,15 @@ function displayUsers()
     arrEntry.length != 0? clearallBtn.classList.add("active") : clearallBtn.classList.remove("active");
 
     let liShowEntry = "";
-    arrEntry = arrEntry.reverse();
+    // arrEntry = arrEntry.reverse();
     arrEntry.forEach((element, index) => {
-        liShowEntry += `<li ><h4 style="color:white"><b>User ${arrEntry.length-index}: </b></h4><p>Username: ${element.firstName + " " + element.lastName}</p> <p>Email: ${element.email}</p> <p>Gender: ${element.gender}</p> Password: ${element.password} <span onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+        liShowEntry += `<li ><h4 style="color:white"><b>User ${arrEntry.length-index}: </b></h4><p>Username: ${element.firstName + " " + element.lastName}</p> <p>Email: ${element.email}</p> <p>Gender: ${element.gender}<span onclick="editTask(${index})" id="edits"><i class="fas fa-edit"></i></span></p> Password: ${element.password} <span onclick="deleteTask(${index})" id="deletes"><i class="fas fa-trash"></i></span></li>`;
     })
    
     showEntry.innerHTML = liShowEntry;
     getPendingTasks.innerHTML = `${arrEntry.length}`;
+
+    getSubmitButton.value = "Submit";
 }
 
 function deleteTask(index) {
@@ -67,6 +121,27 @@ function deleteTask(index) {
 
     displayUsers();
 }
+
+function editTask(index) {
+    getLocaleStorage = localStorage.getItem("Data Entry");
+    arrEntry= JSON.parse(getLocalStorage);
+
+    // arrEntry = arrEntry.reverse();
+
+    getFname.value = arrEntry[index].firstName;
+    getLname.value = arrEntry[index].lastName;
+    getEmail.value = arrEntry[index].email;
+    selectGender = gender;
+    getPassword.value = arrEntry[index].password;
+    getRepassword.value = arrEntry[index].rePassword;
+    getSubmitButton.value = "Edit";
+    tempIndex = index;
+
+    
+    
+}
+
+
 
 clearallBtn.onclick = () => {
     listArr=[];
